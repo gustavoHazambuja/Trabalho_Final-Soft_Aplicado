452f422b7f94bc12ad29fbb62b5866c3cefa2d8c
Gustavo Henrique <guazambujinha@gmail.com>
2025-10-29 16:21:34 -0300

    Repositório modelado

--- Trabalho_Final_Soft_Aplicado/src/main/java/com/example/domain/entities/Ticket.java
+++ Trabalho_Final_Soft_Aplicado/src/main/java/com/example/domain/entities/Ticket.java
@@ -4,16 +4,20 @@ import java.time.LocalDateTime;
 
 import com.example.domain.enums.TicketStatus;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "tb_ticket")
 @Getter
 @Setter
 @AllArgsConstructor
--- Trabalho_Final_Soft_Aplicado/src/main/java/com/example/infrastructure/repositories/TicketRepository.java
+++ Trabalho_Final_Soft_Aplicado/src/main/java/com/example/infrastructure/repositories/TicketRepository.java
@@ -1,5 +1,12 @@
 package com.example.infrastructure.repositories;
 
-public class TicketRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.domain.entities.Ticket;
+
+public interface TicketRepository extends JpaRepository<Ticket,Long> {
     
+    Optional<Ticket> findTicketByCodigo(String codigo);
 }
